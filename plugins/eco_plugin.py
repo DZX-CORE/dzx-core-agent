eb2ecef489c193783426a5cec556e88fcbdbf9de
DZX-CORE <dzxcore@gmail.com>
2025-06-09 12:08:05 -0300

    Atualizar o eco_plugin.py

--- plugins/eco_plugin.py
+++ plugins/eco_plugin.py
@@ -1,5 +1,5 @@
 # plugins/eco_plugin.py
 def handle(mensagem, historico):
     if "eco" in mensagem.lower():
-        return "Você chamou por eco? Estou aqui!"
+        return "ECO: " + mensagem
     return None
