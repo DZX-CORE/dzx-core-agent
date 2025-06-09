fc91e4980662aa130f3624be3e591c08f1ce0e67
DZX-CORE <dzxcore@gmail.com>
2025-06-09 20:37:45 -0300

    Atualizar o error_detector.py

--- orquestrador/error_detector.py
+++ orquestrador/error_detector.py
@@ -1,1 +1,14 @@
+import os
 
+class ErrorDetector:
+    def __init__(self, pasta_projeto):
+        self.pasta = pasta_projeto
+
+    def detectar_erro(self):
+        # Simulação: procura arquivo de log de erro (exemplo)
+        caminho_log = os.path.join(self.pasta, "error.log")
+        if os.path.exists(caminho_log):
+            with open(caminho_log, "r") as f:
+                logs = f.read()
+            return logs
+        return None
