8aa3bf0334317ed5afeb2f6525cb778db2524c15
DZX-CORE <dzxcore@gmail.com>
2025-06-09 23:16:14 -0300

    Atualizar o config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
 
+def load_env_vars():
+    load_dotenv()
+    return {
+        "TELEGRAM_TOKEN": os.getenv("TELEGRAM_BOT_TOKEN"),
+        "GITHUB_TOKEN": os.getenv("GITHUB_TOKEN"),
+        "CLAUDE_API_KEY": os.getenv("CLAUDE_API_KEY")
+    }
