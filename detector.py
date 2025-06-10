bd814cf5d7ba3372826b99240a6d2adef29cbe3f
DZX-CORE <dzxcore@gmail.com>
2025-06-09 23:17:46 -0300

    Atualizar o detector.py

--- detector.py
+++ detector.py
@@ -1,1 +1,14 @@
+import os
+import py_compile
 
+def detect_errors(path):
+    errors = []
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file.endswith(".py"):
+                full_path = os.path.join(root, file)
+                try:
+                    py_compile.compile(full_path, doraise=True)
+                except py_compile.PyCompileError as e:
+                    errors.append(str(e))
+    return errors
